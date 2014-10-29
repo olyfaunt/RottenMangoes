e4ef75c4fbcb76b77089125184fa83f2d237b531
Audrey Jun <audrey.jun@gmail.com>
2014-10-29 01:59:02 -0700

    Initial Commit

---
+++ RottenMangoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenMangoes
+//
+//  Created by Audrey Jun on 2014-10-29.
+//  Copyright (c) 2014 audreyjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
