8916ad8b7722b578af902f3430f2c6f204ab99da
Audrey Jun <audrey.jun@gmail.com>
2014-10-30 01:57:24 -0700

    added mapviewcontroller

---
+++ MapViewController.h
@@ -0,0 +1,15 @@
+//
+//  MapViewController.h
+//  RottenMangoes
+//
+//  Created by Audrey Jun on 2014-10-30.
+//  Copyright (c) 2014 audreyjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+- (IBAction)goBack:(id)sender;
+
+@end
---
+++ MapViewController.m
@@ -0,0 +1,45 @@
+//
+//  MapViewController.m
+//  RottenMangoes
+//
+//  Created by Audrey Jun on 2014-10-30.
+//  Copyright (c) 2014 audreyjun. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)goBack:(id)sender {
+    [self dismissSelf];
+}
+
+- (void)dismissSelf {
+    [self.presentingViewController dismissViewControllerAnimated:UITableViewRowAnimationRight completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
